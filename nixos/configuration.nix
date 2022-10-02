707676f716330b42e8a4cf92a6b725faf92a3f0f
Naman1997 <aroranaman17@gmail.com>
2022-10-02 21:25:03 +0530

    Nixos configs

---
+++ nixos/configuration.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+let
+  kubeMasterIP = "192.168.122.162";
+  kubeMasterHostname = "kube-master";
+  kubeMasterAPIServerPort = 6443;
+in
+{
+  # resolve master hostname
+  networking.extraHosts = "${kubeMasterIP} ${kubeMasterHostname}";
+
+  # packages for administration tasks
+  environment.systemPackages = with pkgs; [
+    kompose
+    kubectl
+    kubernetes
+  ];
+
+  services.kubernetes = {
+    roles = ["master" "node"];
+    masterAddress = kubeMasterHostname;
+    apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
+    easyCerts = true;
+    apiserver = {
+      securePort = kubeMasterAPIServerPort;
+      advertiseAddress = kubeMasterIP;
+    };
+
+    # use coredns
+    addons.dns.enable = true;
+  };
+}
