d3035045a48f5b60964ba6bf7475e3b0a2d4e0d5
Naman Arora <aroranaman17@outlook.com>
2023-01-02 11:42:02 +0530

    Resize disk by +40G in template

--- template.sh
+++ template.sh
@@ -3,6 +3,7 @@ sleep 5
 qm create 7000 --memory 2048 --net0 virtio,bridge=vmbr0 --agent 1
 qm importdisk 7000 /root/fcos-cluster/coreos.qcow2 local-lvm
 qm set 7000 --scsihw virtio-scsi-single --scsi0 local-lvm:vm-7000-disk-0,cache=writeback,discard=on
+qm resize 7000 scsi0 +40G
 qm set 7000 --boot c --bootdisk scsi0
 qm set 7000 --ide2 local-lvm:cloudinit
 qm set 7000 --ciuser core --citype nocloud --ipconfig0 ip=dhcp
